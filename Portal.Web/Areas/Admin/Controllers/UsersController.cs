40bae2f5e80af799d59233dae193cd46275d8b49
parsa mahmoudy <parsa32569@gmail.com>
2019-10-11 01:26:57 +0330

    users management

---
+++ Portal.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Portal.Data.UOW;
+
+namespace Portal.Web.Areas.Admin.Controllers
+{
+    [Area(areaName: "Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UnitOfWork _db;
+        public UsersController(UnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.UsersGenericRepository.Where().ToList());
+        }
+    }
+}
