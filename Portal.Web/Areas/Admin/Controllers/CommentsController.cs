f5023a148dbf9cf6403a25309063d5754999b7f4
parsa mahmoudy <parsa32569@gmail.com>
2019-10-19 10:14:58 +0330

    comments

---
+++ Portal.Web/Areas/Admin/Controllers/CommentsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Portal.Data.UOW;
+
+namespace Portal.Web.Areas.Admin.Controllers
+{
+    [Area(areaName: "Admin")]
+    [Authorize(Roles = "Admin")]
+    public class CommentsController : Controller
+    {
+        private readonly UnitOfWork _db;
+        public CommentsController(UnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.CommentsGenericRepository.Where().ToList());
+        }
+
+        public IActionResult Delete(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                foreach (var item in _db.CommentsGenericRepository.Where(c => c.ParentId == int.Parse(id)))
+                {
+                    _db.CommentsGenericRepository.Delete(int.Parse(id));
+                }
+                _db.CommentsGenericRepository.Delete(int.Parse(id));
+                _db.Save();
+                
+            }
+            return Redirect("/Admin/Comments/Index");
+        }
+    }
+}
---
+++ Portal.Web/ViewComponents/TopPostsComponent/TopPostsComponent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portal.Common.ViewModels.Posts;
+using Portal.Data.UOW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portal.Web.ViewComponents.TopPostsComponent
+{
+    public class TopPostsComponent : ViewComponent
+    {
+        private readonly UnitOfWork _db;
+        public TopPostsComponent(UnitOfWork db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = new List<PostsListViewModel>();
+            foreach (var item in _db.PostsGenericRepository.Where().OrderByDescending(p => p.ViewCount).Take(5))
+            {
+                posts.Add(new PostsListViewModel
+                {
+                    Title = item.Title,
+                    Category = item.Category.Title,
+                    Id = item.Id,
+                    CreatedTime = item.CreatedTime,
+                    Image = item.Image,
+                    Summary = item.Summary
+                });
+            }
+            return await Task.FromResult((IViewComponentResult)
+                View("TopPostsComponent", posts));
+        }
+    }
+}
